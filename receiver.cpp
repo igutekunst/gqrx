c061e31adebd4526aff29e33f3d1d71d170dd410
Pascal Hirsch <Pascal.Hirsch@web.de>
2012-05-12 00:05:31 +0200

    Widened transition width of xlating filter to reduce number of taps (thus cpu load), re-enabled audio.

--- receiver.cpp
+++ receiver.cpp
@@ -63,7 +63,8 @@ receiver::receiver(const std::string input_device, const std::string audio_devic
     iq_fft = make_rx_fft_c(4096, 0);
 
     /** TODO replace fixed internal bandwidth with variable one */
-    const std::vector<float> taps = gr_firdes::low_pass(1,1920000,40000,5000,gr_firdes::WIN_HAMMING,6.76);
+    const std::vector<float> taps = gr_firdes::low_pass(1, d_bandwidth, 40000, 10000, gr_firdes::WIN_HAMMING, 6.76);
+    std::cerr << "xlating filter taps: " << taps.size() << std::endl;
     xlate = gr_make_freq_xlating_fir_filter_ccf(d_bandwidth/d_bandwidth_int, taps, -d_filter_offset, d_bandwidth);
 
     nb = make_rx_nb_cc(d_bandwidth, 3.3, 2.5);
@@ -98,15 +99,11 @@ receiver::receiver(const std::string input_device, const std::string audio_devic
     tb->connect(filter, 0, sql, 0);
     tb->connect(sql, 0, agc, 0);
     tb->connect(agc, 0, demod_fm, 0);
-    tb->connect(demod_fm, 0, audio_gain, 0);
-    tb->connect(demod_fm, 0, audio_fft, 0);
-    //tb->connect(audio_rr, 0, audio_fft, 0);
-    //tb->connect(audio_rr, 0, audio_gain, 0);
+    tb->connect(demod_fm, 0, audio_rr, 0);
+    tb->connect(audio_rr, 0, audio_fft, 0);
+    tb->connect(audio_rr, 0, audio_gain, 0);
 
-    tb->connect(audio_gain, 0, audio_null_sink, 0);
-    /** TODO all hell breaks lose if we connect the audio sink */
-    //tb->connect(audio_gain, 0, audio_snk, 0);
-
+    tb->connect(audio_gain, 0, audio_snk, 0);
 }
 
 
